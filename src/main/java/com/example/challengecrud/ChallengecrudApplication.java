54c1d4a39dccc8453e503934b4afc73b7eed2a99
tavomtorres <gmendeztorres5d@gmail.com>
2021-10-07 19:20:41 -0300

    Initial commit

---
+++ src/main/java/com/example/challengecrud/ChallengecrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/challengecrud/ChallengecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
